7631036a059f4e62134d1bab95aabd5104333a70
yaya <yannisayache@yahoo.com>
2021-10-29 14:36:00 +0200

    [feat] limit  border player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,9 @@ public class PlayerController : MonoBehaviour
 {
     private PlayerInput m_InputsMgr;
     private InputAction m_MoveAction;
+    private Vector3 m_CurrentPos;
+    public float m_XRange = 10f;
+    public float m_ZRange = 10f;
     public float m_Speed = 20f;
     // Start is called before the first frame update
     
@@ -18,12 +21,25 @@ public class PlayerController : MonoBehaviour
     {
         
     }
-
+    private float BorderPlayer( float m_CurrentPos, float m_Range){
+        if(m_CurrentPos > m_Range){
+            m_CurrentPos = m_Range;
+        }
+        else if(m_CurrentPos < - m_Range){
+            m_CurrentPos = m_Range;
+        }
+        return m_CurrentPos;
+    }
     // Update is called once per frame
     void Update()
     {
         Vector2 move = m_MoveAction.ReadValue<Vector2>();
         transform.Translate(Vector3.forward * Time.deltaTime * m_Speed * move.y);
         transform.Translate(Vector3.right * Time.deltaTime * m_Speed * move.x);
+        m_CurrentPos = transform.position;
+        m_CurrentPos.x = BorderPlayer(m_CurrentPos.x,m_XRange);
+        m_CurrentPos.z = BorderPlayer(m_CurrentPos.z,m_ZRange);
+        
+        transform.position = m_CurrentPos;
     }
 }
